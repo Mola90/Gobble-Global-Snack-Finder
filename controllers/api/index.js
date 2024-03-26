2b64d2a9e197dec53bbd2ee153ab44f28d2de8be
Samantha Samarua <samantha.samarua@gmail.com>
2024-03-26 18:23:17 +0930

    Resolved conflicts in API/index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const router = require('express').Router();
 const commentRoutes = require('./comment-route');
 const ratingRoutes = require('./rating-route');
@@ -6,20 +5,10 @@ const searchRoutes = require('./search-route');
 const snackRoutes = require('./snack-route');
 const userRoutes = require('./user-route');
 
-
 router.use('/comment', commentRoutes);
 router.use('/rating', ratingRoutes);
 router.use('/search', searchRoutes);
 router.use('/snack', snackRoutes);
 router.use('/user', userRoutes);
 
-
-=======
-const router = require('express').Router();
-const snackRoutes = require('./snack-route');
-
-
-router.use('/snack', snackRoutes);
-
->>>>>>> main
 module.exports = router;
