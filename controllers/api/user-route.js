4c1870fc83cd27a13d968cbd25a497968464a77c
Samantha Samarua <samantha.samarua@gmail.com>
2024-03-28 18:32:18 +0930

    Updated login route

--- controllers/api/user-route.js
+++ controllers/api/user-route.js
@@ -12,4 +12,33 @@ router.get('/', async (req, res) => {
     }
   });
 
+
+  router.post('/login', async (req, res) => {
+    try {
+      const userData = await User.findOne({ where: { email: req.body.email } });
+  
+      if (!userData) {
+        res.status(400).json({ message: 'No user found with this email address!' });
+        return;
+      }
+  
+      const isValidPassword = await userData.checkPassword(req.body.password);
+      if (!isValidPassword) {
+        res.status(400).json({ message: 'Incorrect password!' });
+        return;
+      }
+  
+      req.session.save(() => {
+        req.session.loggedIn = true;
+        req.session.userId = userData.id;
+        req.session.username = userData.username;
+        res.status(200).json({ user: userData, message: 'You are now logged in!' });
+      });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json(err);
+    }
+  });
+  
+
   module.exports = router;
