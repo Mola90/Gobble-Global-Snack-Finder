0a12c80b931c45436aad67e0b5f34de0e633a619
Samantha Samarua <samantha.samarua@gmail.com>
2024-03-28 23:50:36 +0930

    Started updating Dashboard_Review

--- controllers/api/user-route.js
+++ controllers/api/user-route.js
@@ -38,9 +38,6 @@ router.post('/signup', async (req,res) => {
 
 });
 
-module.exports = router;
-
-
   router.post('/login', async (req, res) => {
     try {
       const userData = await User.findOne({ where: { email: req.body.email } });
