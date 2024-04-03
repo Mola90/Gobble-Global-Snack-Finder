67bcf784dd30883b6b199e645549c90fe94af6a8
KL <karl.w.langer@gmail.com>
2024-04-03 18:35:50 +1030

    test

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,6 @@ const router = require('express').Router();
 const homeRoutes = require('./home-routes');
 const apiRoutes = require('./api');
 const dashboard = require('./dashboard-routes');
-const browse = require('./browse-routes');
 
 router.use('/',homeRoutes);
 router.use('/dashboard', dashboard);
